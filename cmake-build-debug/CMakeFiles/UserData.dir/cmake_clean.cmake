6357a05dd1ed06e7026f7e49b7ad307a9961a55d
DubbSwitch <72952746+DubbSwitch@users.noreply.github.com>
2021-04-05 21:56:05 -0600

    Init Commit

---
+++ cmake-build-debug/CMakeFiles/UserData.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UserData.dir/main.cpp.obj"
+  "UserData.exe"
+  "UserData.exe.manifest"
+  "UserData.pdb"
+  "libUserData.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UserData.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/UserData.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UserData.
+# This may be replaced when dependencies are built.
